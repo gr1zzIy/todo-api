f19917da0286512bd226055703f26b196120f31b
Oleksii Ishchenko <oleksiy26032005@gmail.com>
2026-02-02 22:51:57 +0200

    rename main table in DB

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -7,5 +7,5 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
     
-    public DbSet<TodoItem> TodoItems { get; set;  } = null!;
+    public DbSet<TodoItem> Todos { get; set;  } = null!;
 }
