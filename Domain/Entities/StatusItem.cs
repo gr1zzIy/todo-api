19d100a2f69c01145e37e26df4ad47b242b8db4d
Oleksii Ishchenko <oleksiy26032005@gmail.com>
2026-02-03 22:36:59 +0200

    add Status item and enum

---
+++ Domain/Entities/StatusItem.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using todoApi.Domain.Enums;
+
+namespace todoApi.Domain.Entities;
+
+public sealed class StatusItem
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public StatusItemType Id { get; set; }
+    
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = string.Empty;
+    
+    public ICollection<TodoItem> TodoItems { get; set; } = new List<TodoItem>();
+    
+    public StatusItem(StatusItemType id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+    
+    private StatusItem() { }
+    
+    public static IEnumerable<StatusItem> GetAll()
+    {
+        return
+        [
+            new StatusItem(StatusItemType.Pending, "Pending"),
+            new StatusItem(StatusItemType.InProgress, "In Progress"),
+            new StatusItem(StatusItemType.Completed, "Completed")
+        ];
+    }
+}
---
+++ Domain/Enums/StatusItemType.cs
@@ -0,0 +1,8 @@
+﻿namespace todoApi.Domain.Enums;
+
+public enum StatusItemType : int
+{
+    Pending = 0,
+    InProgress = 1,
+    Completed = 2 
+}
