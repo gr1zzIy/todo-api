48e9069fd6ea307a2379d8a687e61da2ed960e63
Oleksii Ishchenko <oleksiy26032005@gmail.com>
2026-02-03 22:39:01 +0200

    add: extensions for TodoItem

---
+++ Domain/Extensions/TodoItemExtensions.cs
@@ -0,0 +1,31 @@
+﻿using todoApi.Domain.Entities;
+using todoApi.Domain.Enums;
+
+namespace todoApi.Domain.Extensions;
+
+public static class TodoItemExtensions
+{
+    public static bool IsPending(this TodoItem todo) 
+        => todo.StatusId == StatusItemType.Pending;
+    
+    public static bool IsInProgress(this TodoItem todo) 
+        => todo.StatusId == StatusItemType.InProgress;
+    
+    public static bool IsCompleted(this TodoItem todo) 
+        => todo.StatusId == StatusItemType.Completed;
+    
+    public static void MarkAsPending(this TodoItem todo)
+    {
+        todo.StatusId = StatusItemType.Pending;
+    }
+    
+    public static void MarkAsInProgress(this TodoItem todo)
+    {
+        todo.StatusId = StatusItemType.InProgress;
+    }
+    
+    public static void MarkAsCompleted(this TodoItem todo)
+    {
+        todo.StatusId = StatusItemType.Completed;
+    }
+}
